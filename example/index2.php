91098945cae93f1028084e364eacc75da4bd5d89
Malik Umer Farooq <Lablnet01@gmail.com>
2018-12-05 01:12:56 +0000

    Apply fixes from StyleCI

--- example/index2.php
+++ example/index2.php
@@ -1,14 +1,15 @@
 <?php
 use Lablnet\Files;
+
 require_once '../vendor/autoload.php';
 $files = new Files();
 
 if (isset($_POST['submit'])) {
     //File Upload
-        $status = $files->filesUpload($_FILES['file'],'/','image',count($_FILES['file']['name']));
-        foreach ($status as $key => $value) {
-            var_dump($value);
-        }
+    $status = $files->filesUpload($_FILES['file'], '/', 'image', count($_FILES['file']['name']));
+    foreach ($status as $key => $value) {
+        var_dump($value);
+    }
 }
 ?>
 <!DOCTYPE html>
